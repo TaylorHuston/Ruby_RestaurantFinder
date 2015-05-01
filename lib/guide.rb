dfce6e42adb317b7a9441bffa0cf7c624f88dfab
Taylor Huston <taylor.a.huston@gmail.com>
2015-04-30 18:08:26 -0700

    Create Guide class with outline

---
+++ lib/guide.rb
@@ -0,0 +1,20 @@
+class Guide
+  
+  #Locate restaurant text file, or create a new one if needed
+  def initialize(path=nil)
+    
+  end
+  
+  def launch
+    
+  end
+  
+  def introduction
+    
+  end
+  
+  def conclusion
+    
+  end
+  
+end
